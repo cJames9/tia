2d220a4392e5c427c4fce50a5ff170350d43202b
Cleison de Oliveira Padilha <cleison_lp@hotmail.com>
2024-08-19 09:31:03 -0300

    Revert "Merge branch 'master' of https://github.com/cJames9/tia"
    This reverts commit e6046ee2855f3bb167cc4d9f93ae2386154696d8, reversing
    changes made to 83c6b601f264555d7f4979c0a5612d57cfe0856b.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PACKAGE = 'tia'
 PACKAGE_DESC = 'Toolkit for integration and analysis'
 VERSION = tia.__version__
 URL = "https://github.com/bpsmith/tia"
-REQUIRED = ['pandas', 'numpy', 'blpapi']
+REQUIRED = ['pandas', 'numpy']
 REQUIRED_FOR_TESTS = []
 
 LONG_DESC = """\
